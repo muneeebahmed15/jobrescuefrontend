18941877594c118836c33ba24f11359e6a1a9df7
muneeebahmed15 <muneebahme999@gmail.com>
2024-04-18 22:16:32 +0500

    aca

--- src/Components/layout/Header.jsx
+++ src/Components/layout/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { MenuOutlined, LoginOutlined } from '@ant-design/icons';
+import { UserLogin } from '../../actions/authentication';
 
 const Header = () => {
+  const {logout} = UserLogin();
   return (
     <div className='header d-flex justify-content-between align-items-center px-4' style={{marginLeft: "0"}}>
       
@@ -12,7 +14,7 @@ const Header = () => {
         <h4>Welcome, Admin</h4>
 
         <div>
-        <LoginOutlined style={{fontSize: "25px"}}/>
+        <LoginOutlined style={{fontSize: "25px"}} onClick={logout}/>
         </div>
     </div>
   )
