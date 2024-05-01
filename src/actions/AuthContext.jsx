ee58ab005c2bd796c43ec1f14ce6b99eec946a74
muneeebahmed15 <muneebahme999@gmail.com>
2024-05-02 00:14:33 +0500

    cc

--- src/actions/AuthContext.jsx
+++ src/actions/AuthContext.jsx
@@ -18,8 +18,8 @@ const AuthProvider = ({children}) => {
       }
     },[])
 
-    //        https://rescuebackend.vercel.app/v1/rescue      
-    axios.defaults.baseURL = "http://localhost:5000/v1/rescue";
+    //            http://localhost:5000/v1/rescue
+    axios.defaults.baseURL = "https://rescue-three.vercel.app/v1/rescue";
     axios.defaults.headers.common["Cookies"] = auth.token;
 
   return <AuthContext.Provider value={{auth, setAuth}}>{children}</AuthContext.Provider>
